ea0eaeec4390f43026ce39d794974c7f1a0c1e5b
dashprintingservices <dashprintingservices@gmail.com>
2020-02-29 23:51:17 +0800

    Create a migration table. and set an auth in every view

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -28,26 +28,31 @@ class MainController extends Controller
 
     public function clients()
     {
+        $this->middleware("auth");
         return view('clients');
     }
 
     public function sales()
     {
+        $this->middleware("auth");
         return view('sales');
     }
 
     public function inventory()
     {
+        $this->middleware("auth");
         return view('inventory');
     }
 
     public function idProcessing()
     {
+        $this->middleware("auth");
         return view('id_processing');
     }
 
     public function reports()
     {
+        $this->middleware("auth");
         return view('reports');
     }
 }
